fbcd6911ea0e08d52c61dc959cebaab92a930e03
LeonardoJFN <leonardojonathanfn@gmail.com>
2026-02-10 18:51:12 +0700

    tugas week1

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>
+        Leonardo Jonathan fernandez Namlay - 00000058084
+      </Text>
     </View>
   );
 }
@@ -11,11 +13,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
   },
 });
