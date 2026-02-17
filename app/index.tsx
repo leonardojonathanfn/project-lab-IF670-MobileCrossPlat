17b619109b74e4b2aa922657e1ed98ae346fba0a
LeonardoJFN <leonardojonathanfn@gmail.com>
2026-02-17 18:54:31 +0700

    tugas week 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,67 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/friend1.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.text}>Feli Rafael</Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/friend3.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.text}>Aurelia Ivanka</Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/friend6.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.text}>PAtrcik Winardi</Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/friend2.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.text}>Ivan Christian</Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/friend4.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.text}>Vieri Halim</Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/friend5.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.text}>Celine Devany</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
   },
 });
